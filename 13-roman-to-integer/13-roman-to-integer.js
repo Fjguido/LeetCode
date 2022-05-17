2b05b412235e379188eedbdec766958b40bed59f
FJG <89875445+Fjguido@users.noreply.github.com>
2022-05-17 11:24:00 -0400

    Time: 229 ms (23.23%), Space: 46.8 MB (82.15%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -2,42 +2,30 @@
  * @param {string} s
  * @return {number}
  */
-
-const romanHash = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
-  M: 1000,
-};
-const s = "MCMLXXXIX";
-// s = 1989
-function romanToInt(s) {
-  let accumulator = 0;
-for (let i = 0; i < s.length; i++) {
-    if (s[i] === "I" && s[i + 1] === "V") {
-      accumulator += 4;
-      i++;
-    } else if (s[i] === "I" && s[i + 1] === "X") {
-      accumulator += 9;
-      i++;
-    } else if (s[i] === "X" && s[i + 1] === "L") {
-      accumulator += 40;
-      i++;
-    } else if (s[i] === "X" && s[i + 1] === "C") {
-      accumulator += 90;
-      i++;
-    } else if (s[i] === "C" && s[i + 1] === "D") {
-      accumulator += 400;
-      i++;
-    } else if (s[i] === "C" && s[i + 1] === "M") {
-      accumulator += 900;
-      i++;
-    } else {
-      accumulator += romanHash[s[i]];
+var romanToInt = function(s) {
+    const value = { 
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
-  }
-  return accumulator;
-}
+
+    let result = 0;
+
+    for (i=0; i < s.length; i++){
+        const current = value[s[i]];
+        const next = value[s[i+1]];
+
+        if (current < next){
+            result += next - current // IV -> 5 - 1 = 4
+            i++
+        } else {
+            result += current
+        }
+    }
+
+    return result; 
+};

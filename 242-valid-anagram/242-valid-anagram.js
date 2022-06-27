0157893f805cceb8bab88e1dc0d27f5671d179d7
FJG <89875445+Fjguido@users.noreply.github.com>
2022-06-27 16:35:28 -0400

    Time: 128 ms (40.43%), Space: 43.4 MB (72.13%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -3,15 +3,22 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(stringA, stringB) {
-    // Sanitizing
-    // dont understand this
-  stringA = stringA.toLowerCase().replace(/[\W_]+/g, "");
-  stringB = stringB.toLowerCase().replace(/[\W_]+/g, "");
+var isAnagram = function(s, t) {
+  var lenA = s.length;
+  var lenB = t.length;
+  var map = {};
 
-  // sorting
-  const stringASorted = stringA.split("").sort().join("");
-  const stringBSorted = stringB.split("").sort().join("");
+  if (lenA !== lenB) return false;
 
-  return stringASorted === stringBSorted;
+  for (var i = 0; i < lenA; i++) {
+    if (!map[s[i]]) map[s[i]] = 0;
+    map[s[i]]++;
+  }
+
+  for (var j = 0; j < lenB; j++) {
+    if (!map[t[j]]) return false;
+    map[t[j]]--;
+  }
+
+  return true;
 };

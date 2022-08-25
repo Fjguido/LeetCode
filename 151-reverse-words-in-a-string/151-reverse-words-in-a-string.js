e87a355228bbbfa416ae533eb2211815857db924
FJG <89875445+Fjguido@users.noreply.github.com>
2022-08-25 13:04:05 -0400

    Time: 114 ms (24.45%), Space: 43.6 MB (93.06%) - LeetHub

---
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+     let onlyWords = s.match(/[^ ]+/g)
+    let result = []
+    for (let word = 0; word < onlyWords.length; word++) {
+        result.unshift(onlyWords[word])
+    }
+
+    return result.join(" ")
+};

63370d9178e2f014b086fedd4728ff5fd2ec2ff1
FJG <89875445+Fjguido@users.noreply.github.com>
2022-09-19 15:57:09 -0400

    Time: 108 ms (67.31%), Space: 43.4 MB (24.88%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,14 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   for(let i = 0; i < nums.length; i++){
-      for(let j = i + 1; j < nums.length; j++){
-          if(nums[i] + nums[j] === target)
-          return [i, j]
-      }
+   const complete = {};
+    for(let i=0; i<nums.length; i++){
+        if(complete[nums[i]]>=0){
+            return [complete[nums[i]],i]
+        }
+        complete[target-nums[i]] = i
     }
     
-}
+};
 
 
 

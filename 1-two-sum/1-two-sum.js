c54892c2b5ee76f09032ac38ed525a2fbfffc007
FJG <89875445+Fjguido@users.noreply.github.com>
2022-09-19 16:20:57 -0400

    Time: 161 ms (37.04%), Space: 42.3 MB (80.84%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,17 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-   const viewed = []; // access by index is faster that Map
-    for(let i=0; ;i++){ //according to task description - solution is always present - no need to check <.length or something like this
-        const current = nums[i];
-        const j = viewed[current];
-        if(j !== undefined){
-            return [i, j];
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i +1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target)
+                return [i,j]
         }
-
-        viewed[target - current] =  i; 
     }
-};
+}
 
 
 

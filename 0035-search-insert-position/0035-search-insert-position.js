481acb186989ba518c53576bf250374f73ebfab1
FJG <89875445+Fjguido@users.noreply.github.com>
2022-11-11 12:04:13 -0500

    Time: 92 ms (59.94%), Space: 42.3 MB (46.71%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,25 +4,23 @@
  * @return {number}
  */
 const searchInsert = (nums, target) => {
-    let start = 0;
-    let end = nums.length - 1;
-    
-        while(start <= end) {
-            const mid = Math.floor((start + end)/2);
-        
-            if(nums[mid] < target) {
-            start = mid + 1;
-             } else {
-             end = mid -1;
-             }   
+   
+  for (let index = 0; index < nums.length; index++) {
+    if (target <= nums[index]) {
+      return index;
     }
-    
-    return start;
+  }
+  return nums.length;
 };
 
 // we have an array of numbers in order from smallest to greatest and a target number
 // we want to return the index position of the target number in the array 
 // if the target number is not part of the array, we want to return the index of where it would be in the array
+
+// questions to make sure of - are the numbers in order 
+// are there decimals
+// only whole numbers? 
+// any negative numbers
 
 
 

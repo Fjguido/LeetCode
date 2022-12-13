3dcb3ee61995ffd17abb7aeb4eb613dedff9bba9
FJG <89875445+Fjguido@users.noreply.github.com>
2022-12-13 13:44:25 -0500

    Time: 100 ms (80.08%), Space: 43.4 MB (20.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,17 +3,17 @@
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = (nums, target) => {
-     //hashmap
-    let storage = {};
-   
-    // itereate through every element in array -> extracting the index and the actual number out to compute what im using in the storage device
-    for(let [index, num] of nums.entries()) {
-        if(storage[num] !== undefined) return [storage[num], index];
-        storage[target-num] = index;
+function twoSum(arr, target) {
+  const map = new Map();
+  for (let i = 0; i < arr.length; i++) {
+    const complement = target - arr[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
     }
+    map.set(arr[i], i);
+  }
+  throw new Error("No two sum solution");
 }
-
 
 
 

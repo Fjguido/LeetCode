7a4ca8145f14e86ef34796dd3282f34339a69919
FJG <89875445+Fjguido@users.noreply.github.com>
2023-01-17 10:26:48 -0500

    Time: 69 ms (85.80%), Space: 44.5 MB (36.89%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,17 +3,18 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-  let results = []
-  for(let i = 1; i <= n; i++){
-      if(i % 5 === 0 && i % 3 === 0) {
-          results.push("FizzBuzz")
-      } else if ( i % 3 === 0) {
-          results.push("Fizz")
-      } else if ( i % 5 === 0 ){
-          results.push("Buzz")
-      } else {
-          results.push(i.toString())
-      }
-  }
-    return results;
+  let results = [];
+   for(let i = 1; i<=n; i++){
+       if( i % 5 === 0 && i % 3 === 0){
+           results.push("FizzBuzz")
+       } else if (i % 3 === 0){
+           results.push("Fizz")
+       } else if ( i % 5 === 0){
+           results.push("Buzz")
+       } else {
+           results.push(i.toString())
+       }      
+   }
+     return results
 };
+

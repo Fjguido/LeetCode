506c13b32505aa9f29848f3979c3cab4f7a2b35e
FJG <89875445+Fjguido@users.noreply.github.com>
2023-08-04 15:53:57 -0400

    Time: 82 ms (52.68%), Space: 41.9 MB (93.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,24 +3,11 @@
  * @param {number} target
  * @return {number[]}
  */
-
-const twoSum = (arr, target) => {
-    let result = ""
-    if(result === ''){
-        for(i = 0; i < arr.length ; i++){
-            for(j = 0; j < arr.length; j++){
-                if(i !== j){
-                    if(arr[i] + arr[j] === target) {
-                        result = [i, j]
-                    }
-                }
-            }
+var twoSum = function(nums, target) {
+   for(let i = 0; i < nums.length; i++) {
+        for(let j = i +1; j < nums.length; j++){
+            if(nums[i] + nums[j] === target)
+                return [i,j]
         }
     }
-    return result
-}
-
-
-
-
-
+};
